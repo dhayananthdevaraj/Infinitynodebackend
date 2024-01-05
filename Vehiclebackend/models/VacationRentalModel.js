2d745c8a240c1bc070e405da6dfb68e5803a373e
neojarvis <techadmin@iamneo.ai>
2024-01-05 07:30:34 +0000

    Auto-save-commit-1/5/2024--1:00:33-PM

---
+++ Vehiclebackend/models/VacationRentalModel.js
@@ -0,0 +1,51 @@
+// Example model for a vacation rental property
+
+const mongoose = require('mongoose'); // Example for MongoDB with Mongoose
+
+const vacationRentalSchema = new mongoose.Schema({
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', // Reference to the User model for the property owner
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  propertyType: {
+    type: String,
+    enum: ['House', 'Apartment', 'Villa', 'Cabin', 'Condo', 'Other'],
+    required: true
+  },
+  bedrooms: {
+    type: Number,
+    required: true
+  },
+  bathrooms: {
+    type: Number,
+    required: true
+  },
+  amenities: {
+    type: [String] // An array of strings representing amenities
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  pricePerNight: {
+    type: Number,
+    required: true
+  },
+  photo: {
+    type: [String] // An array of strings representing photo URLs
+  },
+
+});
+
+const VacationRental = mongoose.model('VacationRental', vacationRentalSchema);
+
+module.exports = VacationRental;
